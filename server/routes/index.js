01774fd3198a62b6bebbfe63e3667ec1f45eb5ec
4tnuts <ramanavami32@gmail.com>
2020-02-21 15:10:03 +0700

    delete and put API finished

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const PhoneBook = require('../models/phonebook');
 
 router.get('/', (req, res, next) => {
  PhoneBook.find({}, (err, contacts) => {
-   if(err) return res.status(500).res.send(err);
+   if(err) return res.status(500).send(err);
    res.status(201).json(contacts);
  })
 })
@@ -19,6 +19,23 @@ router.post('/', (req, res, next) => {
     if(err) return res.status(500).send(err);
     res.status(200).json(contact)
   })
+})
+
+router.delete('/:id', (req, res, next) => {
+  const id  = req.params.id;
+  PhoneBook.findOneAndDelete(id, (err, contactId) => {
+    if(err) return res.status(500).send(err)
+    res.status(200).json(contactId);
+  })
+})
+
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  console.log("INI ID ", id);
+  PhoneBook.findByIdAndUpdate(id, {$set: {name : req.body.name, phonenumber : req.body.phonenumber}}, function (err, contact) {
+    if (err) return res.status(500).json(err);
+    res.status(200).json(contact);
+});
 })
 
 
